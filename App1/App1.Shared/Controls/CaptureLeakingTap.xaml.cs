a04a8c41b027f10a03e9cd38679956b70957ba2e
Robert MacLean <robert@sadev.co.za>
2014-10-13 08:59:22 +0200

    Final stage for dem

--- App1/App1.Shared/Controls/CaptureLeakingTap.xaml.cs
+++ App1/App1.Shared/Controls/CaptureLeakingTap.xaml.cs
@@ -13,16 +13,13 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+// The User Control item template is documented at http://go.microsoft.com/fwlink/?LinkId=234236
 
-namespace App1
+namespace App1.Controls
 {
-    /// <summary>
-    /// An empty page that can be used on its own or navigated to within a Frame.
-    /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class CaptureLeakingTap : UserControl
     {
-        public MainPage()
+        public CaptureLeakingTap()
         {
             this.InitializeComponent();
         }
